ccff2ec55fc930bd55e3ddcfa671de9d20c21084
Darlan Cavalcante Moreira <darcamo@gmail.com>
2019-08-13 17:08:01 -0300

    Move Tuple2EqualsMatcher<double, double>::match implementation to header file

--- src/details/matchers_imp.cpp
+++ src/details/matchers_imp.cpp
@@ -1,11 +1,3 @@
 #include "matchers_imp.h"
 
-namespace details {
-
-template <>
-bool Tuple2EqualsMatcher<double, double>::match(const Tuple2 &t) const {
-  return (std::get<0>(this->t) == Approx(std::get<0>(t))) &&
-         (std::get<1>(this->t) == Approx(std::get<1>(t)));
-}
-
-} // namespace details
+namespace details {} // namespace details
--- src/details/matchers_imp.h
+++ src/details/matchers_imp.h
@@ -138,7 +138,10 @@ public:
 // version for the case of a tuple of two doubles. The actual implementation of
 // this specialization is provided in the cpp file.
 template <>
-bool Tuple2EqualsMatcher<double, double>::match(const Tuple2 &t) const;
+bool Tuple2EqualsMatcher<double, double>::match(const Tuple2 &t) const {
+  return (std::get<0>(this->t) == Approx(std::get<0>(t))) &&
+         (std::get<1>(this->t) == Approx(std::get<1>(t)));
+}
 
 template <typename T, typename U>
 bool Tuple2EqualsMatcher<T, U>::match(const Tuple2 &t) const {
