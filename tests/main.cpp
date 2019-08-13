11cef3ce7637992d4d62d179d1c29c928ae84d70
Darlan Cavalcante Moreira <darcamo@gmail.com>
2019-08-13 15:35:03 -0300

    Add tests for the functions in all.h

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,6 +4,7 @@
 #include <catch2/catch.hpp>
 #include <random>
 #include <sstream>
+#include <tuple>
 
 using namespace details;
 using namespace Catch;
@@ -45,8 +46,39 @@ TEST_CASE("Test util functions") {
     }
   }
 
+  SECTION("Test Equals for armadillo types") {
+    arma::vec v1{1.5, 2.7, -5.5};
+    arma::vec v2{1.5, 2.7, -5.5};
+
+    REQUIRE_THAT(v1, Equals(v2));
+
+    arma::vec v2_noisy = v2 + 1e-10;
+    REQUIRE_THAT(v1, !Equals(v2_noisy));
+    REQUIRE_THAT(v1, Equals(v2_noisy, 1e-8));
+    REQUIRE_THAT(v1, EqualsWithinTolerance(v2_noisy, 1e-8));
+  }
+
+  SECTION("Test Equals for tuples with two elements") {
+    auto t1 = std::make_tuple(1, 5);
+    auto t1_other = std::make_tuple(1, 5);
+    REQUIRE_THAT(t1, Equals(t1_other));
+
+    auto t2 = std::make_tuple(1.5, -2.6);
+    auto t2_other = std::make_tuple(1.5, -2.6 + 1e-14);
+    REQUIRE_THAT(t2, Equals(t2_other));
+  }
+
+  SECTION("Test InRange") {
+    REQUIRE_THAT(1.5, InRange(0, 5));
+    REQUIRE_THAT(10.5, !InRange(0, 5));
+
+    REQUIRE_THAT(0, InRange(0, 5));
+    REQUIRE_THAT(5, !InRange(0, 5));
+    REQUIRE_THAT(5.0 - 1e-12, InRange(0.0, 5.0));
+  }
+
   SECTION("Test EqualsAngle") {
-    REQUIRE_THAT(2.5*PI, EqualsAngle(0.5*PI));
+    REQUIRE_THAT(2.5 * PI, EqualsAngle(0.5 * PI));
 
     REQUIRE_THAT(-1e-16, EqualsAngle(1e-16));
   }
@@ -145,30 +177,43 @@ TEST_CASE("Test util functions") {
 
       SECTION("Larger Size") {
         {
-          arma::imat m{{1, 2, 3, 4, 5, 6, 7, 8, 9}, {9, 8, 7, 6, 5, 4, 3, 2, 1}};
+          arma::imat m{{1, 2, 3, 4, 5, 6, 7, 8, 9},
+                       {9, 8, 7, 6, 5, 4, 3, 2, 1}};
           std::string expetedStr_m =
-              "(2x9) -> Only showing from column 0 to 4\n        1        2        3        4        5\n        9        8        7        6        5\n";
+              "(2x9) -> Only showing from column 0 to 4\n        1        2    "
+              "    3        4        5\n        9        8        7        6   "
+              "     5\n";
           REQUIRE_THAT(convert(m), Equals(expetedStr_m));
         }
 
         {
           arma::mat m{{1, 2, 3, 4, 5, 6, 7, 8, 9}, {9, 8, 7, 6, 5, 4, 3, 2, 1}};
           std::string expetedStr_m =
-              "(2x9) -> Only showing from column 0 to 4\n   1.0000   2.0000   3.0000   4.0000   5.0000\n   9.0000   8.0000   7.0000   6.0000   5.0000\n";
+              "(2x9) -> Only showing from column 0 to 4\n   1.0000   2.0000   "
+              "3.0000   4.0000   5.0000\n   9.0000   8.0000   7.0000   6.0000  "
+              " 5.0000\n";
           REQUIRE_THAT(convert(m), Equals(expetedStr_m));
         }
 
         {
-          arma::imat m{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {9, 8, 7}, {6, 5, 4}, {3, 2, 1}, {1, 2, 3}, {4, 5, 6}};
+          arma::imat m{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {9, 8, 7},
+                       {6, 5, 4}, {3, 2, 1}, {1, 2, 3}, {4, 5, 6}};
           std::string expetedStr_m =
-              "(8x3) -> Only showing from row 0 to 5\n        1        2        3\n        4        5        6\n        7        8        9\n        9        8        7\n        6        5        4\n        3        2        1\n";
+              "(8x3) -> Only showing from row 0 to 5\n        1        2       "
+              " 3\n        4        5        6\n        7        8        9\n  "
+              "      9        8        7\n        6        5        4\n        "
+              "3        2        1\n";
           REQUIRE_THAT(convert(m), Equals(expetedStr_m));
         }
 
         {
-          arma::mat m{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {9, 8, 7}, {6, 5, 4}, {3, 2, 1}, {1, 2, 3}, {4, 5, 6}};
+          arma::mat m{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {9, 8, 7},
+                      {6, 5, 4}, {3, 2, 1}, {1, 2, 3}, {4, 5, 6}};
           std::string expetedStr_m =
-              "(8x3) -> Only showing from row 0 to 5\n   1.0000   2.0000   3.0000\n   4.0000   5.0000   6.0000\n   7.0000   8.0000   9.0000\n   9.0000   8.0000   7.0000\n   6.0000   5.0000   4.0000\n   3.0000   2.0000   1.0000\n";
+              "(8x3) -> Only showing from row 0 to 5\n   1.0000   2.0000   "
+              "3.0000\n   4.0000   5.0000   6.0000\n   7.0000   8.0000   "
+              "9.0000\n   9.0000   8.0000   7.0000\n   6.0000   5.0000   "
+              "4.0000\n   3.0000   2.0000   1.0000\n";
           REQUIRE_THAT(convert(m), Equals(expetedStr_m));
         }
         /// @todo Implement-me
