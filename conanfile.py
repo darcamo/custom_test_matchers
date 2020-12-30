c981e43415445d7f19ed3eac748b4d2320140d98
Darlan Cavalcante Moreira <darcamo@gmail.com>
2020-12-30 18:01:22 -0300

    Add conan recipe to this repository
    Now instead of keeping the conan recipe in a separated repository it is in this
    repository and uses the `exports_sources = "*"` option in the recipe.
    
    See https://docs.conan.io/en/latest/creating_packages/package_repo.html#recipe-and-sources-in-the-same-repo

---
+++ conanfile.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+
+from conans import CMake, ConanFile, tools
+
+
+class CustomtestmatchersConan(ConanFile):
+    name = "custom_test_matchers"
+    version = "1.0.0"
+    license = "GTEL Licence"
+    author = "Darlan Cavalcante Moreira <darcamo@gmail.com>"
+    url = "https://github.com/darcamo/conan-custom_test_matchers"
+    description = "Custom catch2 test matchers for armadillo and other types."
+    topics = ("C++", "catch", "tests")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake"
+    exports_sources = "*"
+
+    def requirements(self):
+        self.requires("Catch2/[>=2.11.0]@catchorg/stable")
+        self.requires("fmt/[>=6.0.0]@bincrafters/stable")
+        self.requires("armadillo/[>=10.1.2]@darcamo/stable")
+        if self.options.shared:
+            self.options["armadillo"].shared = True
+        # self.options["armadillo"].use_extern_cxx11_rng = True
+        # self.options["armadillo"].use_system_blas = True
+        # self.options["armadillo"].use_system_hdf5 = True
+
+    def build(self):
+        cmake = CMake(self)
+        os.mkdir("build")
+        shutil.move("conanbuildinfo.cmake", "build/")
+        cmake.configure(source_folder=".", build_folder="build")
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", dst="include/custom_test_matchers", src="src/")
+        self.copy("*.a", dst="lib", src="build/lib/",keep_path=False)
+        self.copy("*.so", dst="lib", src="build/lib/", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["custom_test_matchers"]
+        self.cpp_info.includedirs.append("custom_test_matchers/fivegchannel")
---
+++ test_package/example.cpp
@@ -0,0 +1,21 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include "custom_test_matchers/all.h"
+#include <armadillo>
+
+using namespace Catch;
+
+constexpr const double PI =
+    3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
+
+TEST_CASE("Tests using the catch library") {
+  REQUIRE_THAT(2.5 * PI, EqualsAngle(2.5 * PI));
+  REQUIRE_THAT(-1.5 * PI, EqualsAngle(-1.5 * PI));
+  REQUIRE_THAT(-1.5 * PI, EqualsAngle(0.5 * PI));
+
+  arma::ivec v1{1, 2, 3, 4, 5};
+  arma::ivec v2{1, 2, 3, 4, 5};
+
+  REQUIRE_THAT(v1, Equals(v2));
+}
